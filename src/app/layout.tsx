f4a3b5096c83f3736b64785bb831c137d16adc22
TheWinci <leniwyinzynier@gmail.com>
2025-02-23 20:34:50 +0100

    feat: 🎸 added vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import "./globals.css";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
@@ -39,6 +40,7 @@ export default function RootLayout({
               <SidebarInset>{children}</SidebarInset>
             </SidebarProvider>
           </Providers>
+          <SpeedInsights />
         </body>
       </html>
     </>
